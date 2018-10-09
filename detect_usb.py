0bb2893c7e884b1f2658a2baa7ed09898a324bcf
csapersson <simon_persson@outlook.com>
2018-10-09 12:32:59 +0200

    asdf

--- detect_usb.py
+++ detect_usb.py
@@ -1,14 +1,12 @@
 import pyudev
 
-print("Script is running...")
-
 context = pyudev.Context()
 monitor = pyudev.Monitor.from_netlink(context)
 monitor.filter_by(subsystem="usb")
 
-for device in iter(monitor.poll, None):
-    if device.action == "add":
+for action, device in monitor:
+    if action == "add":
         print("Device connected.")
-
-    elif device.action == "remove":
+        
+    elif action == "remove":
         print("Device disconnected.")
